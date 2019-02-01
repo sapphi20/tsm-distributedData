aeb5ae0162a8e95c8a9e3652a91099a98a06e658
Alejandra Alarcón <alalarco@dcc.uchile.cl>
2019-02-01 17:55:06 +0000

    It works!

--- Scripts/test_cass.py
+++ Scripts/test_cass.py
@@ -1,17 +1,13 @@
 # Configuratins related to Cassandra connector & Cluster
-import os
-from pyspark import SparkContext
+# import os
+# Esto funcionaba con 1 IP, no sé si con varias
+# os.environ['PYSPARK_SUBMIT_ARGS']='--packages com.datastax.spark:spark-cassandra-connector_2.11:2.3.0 --conf spark.cassandra.connection.host=192.168.101.140 pyspark-shell'
+from pyspark import SparkContext, SparkConf
 from pyspark.sql import SQLContext
-
-os.environ['PYSPARK_SUBMIT_ARGS'] =\
-'--packages com.datastax.spark:spark-cassandra-connector_2.11:2.3.0\
---conf spark.cassandra.connection.host=192.168.101.140, 192.168.101.141,\
-192.168.101.142, 192.168.101.143 pyspark-shell'
-
-sc = SparkContext("local", "movie lens app")
+#from pyspark_cassandra import CassandraSparkContext, Row
+sc = SparkContext()
 sqlContext = SQLContext(sc)
 
-# Loads and returns data frame por a table including key space given
 def load_and_get_table_df(keys_space_name, table_name):
     table_df = sqlContext.read\
         .format("org.apache.spark.sql.cassandra")\
@@ -19,7 +15,6 @@ def load_and_get_table_df(keys_space_name, table_name):
         .load()
     return table_df
 
-movies = load_and_get_table_df("movie_lens", "movies")
-ratings = load_and_get_table_df("movie_lens", "ratings")
+test1 = load_and_get_table_df("test", "test1")
+test1.show()
 
-movies.show()
