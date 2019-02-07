661aaf96f98aff2f5f149cbe325d028a457663ba
Alejandra Alarcón <alalarco@dcc.uchile.cl>
2019-02-07 15:09:35 +0000

    Edited to actually insert data

--- Scripts/cassandra_insert.py
+++ Scripts/cassandra_insert.py
@@ -1,19 +1,34 @@
 #!/usr/bin/python3
 import random
 from cassandra.cluster import Cluster
+from pyspark import SparkContext
 
 # Configuracion para conectar al cluster
 # se pueden omitir algunos nodos
 ips = ['192.168.101.140', '192.168.101.141', '192.168.101.142', '192.168.101.143']
 cluster = Cluster(ips)
 
+def insert_data(data):
+	from cassandra.cluster import Cluster
+	from cassandra import AlreadyExists
+	# Conexion al cluster de Cassandra al keyspace "test"
+	session = Cluster(['192.168.101.140', '192.168.101.141', '192.168.101.142', '192.168.101.143']).connect("test")
+	try:
+		# Primero crea la tabla
+		session.execute("CREATE TABLE cooperativa (id text PRIMARY KEY, cuerpo text)")
+		# Inserta datos 
+		insertar = session.prepare("INSERT INTO cooperativa (id, cuerpo) VALUES (?, ?)")
+		for d in data.collect():
+			session.execute(insertar, (d[0], d[1]))
+	except AlreadyExists:
+		# Inserta datos a la tabla ya existente
+		insertar = session.prepare("INSERT INTO cooperativa (id, cuerpo) VALUES (?, ?)")
+		for d in data.collect():
+			session.execute(insertar, (d[0], d[1]))
 
 if __name__ == '__main__':
-	# Establece conexion con una sesion
-	# connect toma un argumento opcional con el keyspace a eleccion
-	session = cluster.connect('test')
-	rand_list = [random.randint(0, 100) for i in range(100)]
-	# El metodo execute ejecuta queries
-	for j in range(100):
-		session.execute("INSERT INTO test1 (col1, col2) VALUES (%(col1)s, %(col2)s)"
-			,{'col1': str(j), 'col2': rand_list[j]})
+	sc = SparkContext()
+	rdd = sc.wholeTextFiles('hdfs:///user/hadoop/cooperativa/2015*')
+	# Quita los textos vacios
+	rdd = rdd.filter(lambda key: key[1] is not '')
+	insert_data(rdd)
