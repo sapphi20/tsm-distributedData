5416791d21c9b2e0a0abd2791d54b6e73e1b564f
Alejandra Alarcón <alalarco@dcc.uchile.cl>
2019-02-12 19:09:12 +0000

    Added conversion from RDD to DataFrame

--- Scripts/tfidf_cassandra.py
+++ Scripts/tfidf_cassandra.py
@@ -1,46 +1,44 @@
 #!/usr/bin/python3
-from pyspark import SparkContext, SparkConf
+from pyspark import SparkContext
 from pyspark.mllib.feature import HashingTF, IDF
+from pyspark.mllib.linalg import SparseVector
 from pyspark.sql import SQLContext
 from pyspark_cassandra import CassandraSparkContext, Row
+import numpy
+import pandas
 
 # Configuracion
 sc = CassandraSparkContext()
 sqlContext = SQLContext(sc)
-'''
-# Carga una tabla en un keyspace a un Dataframe
+
+
+# Carga una tabla en un keyspace determinado
+# retorna un dataframe (que después se puede pasar a un RDD)
 def load_and_get_table_df(keys_space_name, table_name):
     table_df = sqlContext.read\
         .format("org.apache.spark.sql.cassandra")\
         .options(table=table_name, keyspace=keys_space_name)\
         .load()
     return table_df
-'''
-# Inserta datos desde HDFS leidos con wholeTextFiles (para obtener pares llave-valor)
-# en una tabla dentro del cluster de Cassandra
 
-
+def rdd_to_df(data):
+	new_data = data.map(lambda row: (row, ))
+	return sqlContext.createDataFrame(new_data)
 
 if __name__ == '__main__':
-	rdd = sc.wholeTextFiles('hdfs:///user/hadoop/cooperativa/2012*')
-	# Quita los textos vacios
-	rdd = rdd.filter(lambda key: key[1] is not '')
-	# Inserta filas a tablas en cassandra
-	# insert_data(rdd)
-	
 	# Carga la tabla en un dataframe
-	###tabla = load_and_get_table_df("test", "corpus")
-	tabla = sc.cassandraTable("test", "cooperativa")
+	tabla = load_and_get_table_df("test", "cooperativa")
+	#print(tabla.head())
+	#tokenizer = Tokenizer(inputCol="cuerpo", outputCol="words")
+	#wordsData = tokenizer.transform(tabla)
 	# Convierte dataframe a RDD y quita los saltos de linea
-	#tabla = tabla.rdd.map(tuple)
-	print(type(tabla))
-	'''
+	tabla = tabla.rdd.map(tuple).map(lambda line: line[1])
+	# Nos deja con solo los cuerpos de las noticias
 	# Comienza calculo de TF-IDF
 	hashing = HashingTF()
 	tf = hashing.transform(tabla)
 	idf = IDF().fit(tf)
 	tfidf = idf.transform(tf)
-
-	for r in tfidf.collect():
-		print(r)
-	'''
+	tfidf_df = rdd_to_df(tfidf)
+	print(type(tfidf_df))
+	print(tfidf.first())
