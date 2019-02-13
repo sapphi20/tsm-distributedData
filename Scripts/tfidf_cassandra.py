f638bcf50598e106aadc68b45f11205c258c3317
Alejandra Alarcón <alalarco@dcc.uchile.cl>
2019-02-13 14:47:47 +0000

    Rearranged code

--- Scripts/tfidf_cassandra.py
+++ Scripts/tfidf_cassandra.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 from pyspark import SparkContext
 from pyspark.mllib.feature import HashingTF, IDF
-from pyspark.mllib.linalg import SparseVector
 from pyspark.sql import SQLContext
 from pyspark_cassandra import CassandraSparkContext, Row
 import numpy
@@ -21,24 +20,28 @@ def load_and_get_table_df(keys_space_name, table_name):
         .load()
     return table_df
 
+# Convierte datos de un RDD a un DataFrame
 def rdd_to_df(data):
 	new_data = data.map(lambda row: (row, ))
 	return sqlContext.createDataFrame(new_data)
 
+# Pasos que procesan data para el calculo de tfidf
+def tfidf(data):
+	hashing = HashingTF()
+	tf = hashing.transform(data)
+	idf = IDF().fit(tf)
+	tfidf = idf.transform(tf)
+	return tfidf
+
 if __name__ == '__main__':
 	# Carga la tabla en un dataframe
 	tabla = load_and_get_table_df("test", "cooperativa")
-	#print(tabla.head())
-	#tokenizer = Tokenizer(inputCol="cuerpo", outputCol="words")
-	#wordsData = tokenizer.transform(tabla)
 	# Convierte dataframe a RDD y quita los saltos de linea
 	tabla = tabla.rdd.map(tuple).map(lambda line: line[1])
 	# Nos deja con solo los cuerpos de las noticias
 	# Comienza calculo de TF-IDF
-	hashing = HashingTF()
-	tf = hashing.transform(tabla)
-	idf = IDF().fit(tf)
-	tfidf = idf.transform(tf)
+	tfidf = tfidf(tabla)
+	#Convierte RDD a DataFrame de spark
 	tfidf_df = rdd_to_df(tfidf)
-	print(type(tfidf_df))
-	print(tfidf.first())
+	# Convierte el DataFrame a uno de pandas
+	tfidf_df.toPandas()
